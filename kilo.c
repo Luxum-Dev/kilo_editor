b73f7085854b6fc8d942fddba932a303d68266d9
Luxum-Dev <coce@netcourrier.com>
2023-02-20 22:01:44 +0100

    Better movement with tab

--- kilo.c
+++ kilo.c
@@ -192,6 +192,17 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t')
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    rx++;
+  }
+  return rx;
+}
+
 void editorUpdateRow(erow *row) {
   int tabs = 0;
   int j;
@@ -273,17 +284,21 @@ void abFree(struct abuf *ab) { free(ab->b); }
 
 /*** output ***/
 void editorScroll() {
+  E.rx = E.cx;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
-  if (E.cx < E.colloff) {
-    E.colloff = E.cx;
+  if (E.rx < E.colloff) {
+    E.colloff = E.rx;
   }
-  if (E.cx >= E.colloff + E.screencols) {
-    E.colloff = E.cx - E.screencols + 1;
+  if (E.rx >= E.colloff + E.screencols) {
+    E.colloff = E.rx - E.screencols + 1;
   }
 }
 
@@ -337,7 +352,7 @@ void editorRefreshScreen() {
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-           (E.cx - E.colloff) + 1);
+           (E.rx - E.colloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
