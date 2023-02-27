fd066509420ab0929771a885f4b96526a993b01e
Luxum-Dev <coce@netcourrier.com>
2023-02-27 21:54:48 +0100

    Implement "dirty" buffer

--- kilo.c
+++ kilo.c
@@ -21,6 +21,7 @@
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
 #define CTRL_KEY(k) ((k)&0x1f)
+#define KILO_QUIT_TIMES 3
 
 // Add it here otherwise it will segfault
 /*** prototypes ***/
@@ -59,6 +60,7 @@ struct editorConfig {
   int screenrows;
   int screencols;
   int numrows;
+  int dirty;
   erow *row;
   char *filename;
   char statusmsg[80];
@@ -255,6 +257,7 @@ void editorAppendRow(char *s, size_t len) {
 
   editorUpdateRow(&E.row[at]);
   E.numrows++;
+  E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
@@ -265,6 +268,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
   row->size++;
   row->chars[at] = c;
   editorUpdateRow(row);
+  E.dirty++;
 }
 
 /*** editor operations ***/
@@ -316,6 +320,7 @@ void editorOpen(char *filename) {
   }
   free(line);
   fclose(fp);
+  E.dirty = 0;
 }
 
 void editorSave() {
@@ -332,6 +337,7 @@ void editorSave() {
         close(fd);
         free(buf);
         editorSetStatusMessage("%d bytes written to disk", len);
+        E.dirty = 0;
         return;
       }
     }
@@ -421,8 +427,9 @@ void editorDrawRows(struct abuf *ab) {
 void editorDrawStatusBar(struct abuf *ab) {
   abAppend(ab, "\x1b[7m", 4);
   char status[80], rstatus[80];
-  int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-                     E.filename ? E.filename : "[No name]", E.numrows);
+  int len = snprintf(status, sizeof(status), "%.20s - %d lines %s",
+                     E.filename ? E.filename : "[No name]", E.numrows,
+                     E.dirty ? "(modified)" : "");
   int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
   if (len > E.screencols)
     len = E.screencols;
@@ -521,12 +528,20 @@ void editorMoveCursor(int key) {
   }
 }
 void editorProcessKeypress() {
+  static int quit_times = KILO_QUIT_TIMES;
   int c = editorReadKey();
 
   switch (c) {
   case '\r':
     break;
   case CTRL_KEY('q'):
+    if (E.dirty && quit_times > 0) {
+      editorSetStatusMessage("WARNING!!! file has not been saved. Press CTRL-Q "
+                             "%d more times to quit.",
+                             quit_times);
+      quit_times--;
+      return;
+    }
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
@@ -578,6 +593,7 @@ void editorProcessKeypress() {
     editorInsertChar(c);
     break;
   }
+  quit_times = KILO_QUIT_TIMES;
 }
 
 /*** init ***/
@@ -590,6 +606,7 @@ void initEditor() {
   E.colloff = 0;
   E.rx = 0;
   E.filename = NULL;
+  E.dirty = 0;
   E.statusmsg[0] = '\0';
   E.statusmsg_time = 0;
   if (getWindowSize(&E.screenrows, &E.screencols) == -1)
